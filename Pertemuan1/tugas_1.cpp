12c3d181bff7f6dbd2a81e28394ffd5c79c57f95
Baby Aisha Maritza Virginia <78396352+LuminetteBourgeons@users.noreply.github.com>
2021-02-22 11:29:47 +0700

    Create tugas_1.cpp

---
+++ Pertemuan1/tugas_1.cpp
@@ -0,0 +1,15 @@
+//Buatlah sebuah program untuk menampilkan data sebagai berikut:
+//  STMIK TOP MARTKOTOP Yogyakarta
+//  Jurusan Teknik Informatika
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    cout<<"STMIK TOP MARTKOTOP Yogyakarta"<<endl;
+    cout<<"Jurusan Teknik Informatika"<<endl;
+}
