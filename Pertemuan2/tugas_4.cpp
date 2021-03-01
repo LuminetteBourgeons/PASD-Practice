fa5ec93c475140585d9473e64ff340a294673fe4
Baby Aisha Maritza Virginia <78396352+LuminetteBourgeons@users.noreply.github.com>
2021-03-01 11:09:57 +0700

    Create tugas_4.cpp

---
+++ Pertemuan2/tugas_4.cpp
@@ -0,0 +1,93 @@
+//WORK IN PROGRESS
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int pilihan;
+    
+    cout<<"Pilih zodiak Anda, "<<endl;
+    cout<<"1. Capricorn"<<endl;
+    cout<<"2. Aquarius"<<endl;
+    cout<<"3. Pisces"<<endl;
+    cout<<"4. Aries"<<endl;
+    cout<<"5. Taurus"<<endl;
+    cout<<"6. Gemini"<<endl;
+    cout<<"7. Cancer"<<endl;
+    cout<<"8. Leo"<<endl;
+    cout<<"9. Virgo"<<endl;
+    cout<<"10. Libra"<<endl;
+    cout<<"11. Scorpio"<<endl;
+    cout<<"12. Sagitarius"<<endl;
+    cout<<"Masukkan pilihan Anda: ";
+    cin>>pilihan;
+    cout<<endl;
+    cout<<endl;
+    
+    
+    switch(pilihan){
+        case 1:
+            cout<<">Capricorn"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 23 Agustus 2021"<<endl;
+            cout<<"Warna kesukaan    : "<<endl;
+        case 2:
+            cout<<"Aquarius"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 11 Februari 2021"<<endl;
+            cout<<"Warna kesukaan    : "<<endl;
+        case 3:
+            cout<<"Pisces"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 21 Juni 2021"<<endl;
+            cout<<"Warna kesukaan    : "<<endl;
+        case 4:
+            cout<<"Aries"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 26 Maret 2021"<<endl;
+            cout<<"Warna kesukaan    : Merah dan Kuning"<<endl;
+        case 5:
+            cout<<"Taurus"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 17 Januari 2021"<<endl;
+            cout<<"Warna kesukaan    : Cokelat dan Putih"<<endl;
+        case 6:
+            cout<<"Gemini"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 31 Mei 2021"<<endl;
+            cout<<"Warna kesukaan    : Jingga dan Kuning"<<endl;
+        case 7:
+            cout<<"Cancer"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 23 Juni 2021"<<endl;
+            cout<<"Warna kesukaan    : Silver dan Biru"<<endl;
+        case 8:
+            cout<<"Leo"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 13 Juli 2021"<<endl;
+            cout<<"Warna kesukaan    : Kuning dan Emas"<<endl;
+        case 9:
+            cout<<"Virgo"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 12 Juli 2021"<<endl;
+            cout<<"Warna kesukaan    : Hijau dan Abu-abu"<<endl;
+        case 10:
+            cout<<"Libra"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 6 September 2021"<<endl;
+            cout<<"Warna kesukaan    : Biru dan Pink"<<endl;
+        case 11:
+            cout<<"Scorpio"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 17 November 2021"<<endl;
+            cout<<"Warna kesukaan    : Hitam dan Merah"<<endl;
+        case 12:
+            cout<<"Sagitarius"<<endl;
+            cout<<endl;
+            cout<<"Hari keberuntungan: 21 Mei 2021"<<endl;
+            cout<<"Warna kesukaan    : Ungu dan Jingga"<<endl;
+
+    }
+}
