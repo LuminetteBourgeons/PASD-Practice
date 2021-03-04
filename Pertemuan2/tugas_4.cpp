9aac20aa006f02aaea115e34df21e959b49e6380
Baby Aisha Maritza Virginia <78396352+LuminetteBourgeons@users.noreply.github.com>
2021-03-04 08:35:06 +0700

    Update tugas_4.cpp

--- Pertemuan2/tugas_4.cpp
+++ Pertemuan2/tugas_4.cpp
@@ -1,4 +1,12 @@
-//WORK IN PROGRESS
+/* 
+    Buatlah program ramalan bintang yang terdiri dari Capricorn, Aquarius, Pisces, 
+    Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, dan Sagitarius yang
+    masing-masing berisi tentang:
+    ~ Hari keberuntungan
+    ~ Warna kesukaan
+    ~ Keuangan
+    ~ Percintaan
+*/
 
 #include <iostream>
 
@@ -35,7 +43,7 @@ int main ()
             cout<<"Hari keberuntungan: 23 Agustus 2021"<<endl;
             cout<<"Warna kesukaan    : Abu-abu dan Cokelat"<<endl;
             cout<<"Ramalan Keuangan  : Pemborosan tetap perlu diwaspadai dan dicegah jangan sampai timbul di hari ini."<<endl;
-            cout<<"Ramalan Percintaan: Zodiak Capricorn, kamu akan bertemu orang baru dan berteman dengannya. Tak disangka, dia menaruh hati padamu. Jika kamu                              tertarik dengannya, jangan ragu-ragu atau kesempatan itu akan                            lenyap. Sedangkan untuk kamu yang sudah punya pasangan, kamu                             sepertinya sedang mengalami masalah yang cukup sulit. Bicarakan                          dengan si dia, ya, jangan disimpan sendirian!"<<endl;
+            cout<<"Ramalan Percintaan: Zodiak Capricorn, kamu akan bertemu orang baru dan berteman dengannya. Tak disangka, dia menaruh hati padamu. Jika kamu tertarik dengannya, jangan ragu-ragu atau kesempatan itu akan lenyap. Sedangkan untuk kamu yang sudah punya pasangan, kamu sepertinya sedang mengalami masalah yang cukup sulit. Bicarakan dengan si dia, ya, jangan disimpan sendirian!"<<endl;
                 break;
         case 2:
             cout<<"Aquarius"<<endl;
@@ -43,7 +51,7 @@ int main ()
             cout<<"Hari keberuntungan: 11 Februari 2021"<<endl;
             cout<<"Warna kesukaan    : Biru dan Abu-abu"<<endl;
             cout<<"Ramalan Keuangan  : Jangan main hantam, jika dirasa ada keraguan sebaiknya ditunda dulu."<<endl;
-            cout<<"Ramalan Percintaan: Tanpa kamu sadari, untuk kamu Zodiak Aquarius pasti merasa sangat bergantung pada pasanganmu. Coba, deh, lihat kembali hubungan                            kalian, jangan sampai kesalahan yang sama membuatmu kembali                              merana. Untuk kamu yang sedang single, kamu merasa kehidupan                             cintamu lagi hambar, nih. Sebenarnya yang kamu butuhkan hanya                            mengubah cara pandang dan menghargai setiap hal kecil dalam                              hidupmu"<<endl;
+            cout<<"Ramalan Percintaan: Tanpa kamu sadari, untuk kamu Zodiak Aquarius pasti merasa sangat bergantung pada pasanganmu. Coba, deh, lihat kembali hubungan kalian, jangan sampai kesalahan yang sama membuatmu kembali merana. Untuk kamu yang sedang single, kamu merasa kehidupan cintamu lagi hambar, nih. Sebenarnya yang kamu butuhkan hanya mengubah cara pandang dan menghargai setiap hal kecil dalam hidupmu"<<endl;
                 break;
         case 3:
             cout<<"Pisces"<<endl;
@@ -51,7 +59,7 @@ int main ()
             cout<<"Hari keberuntungan: 21 Juni 2021"<<endl;
             cout<<"Warna kesukaan    : Putih dan Biru"<<endl;
             cout<<"Ramalan Keuangan  : Apa yang anda tunggu-tunggu kini bisa terealisasi, sehingga cukup membawa angin segar bagi keuangan anda."<<endl;
-            cout<<"Ramalan Percintaan: Bagi kamu Zodiak Pisces yang sedang single, kesempatan kamu untuk menemukan pasangan di tahun ini begitu besar. Jangan lupa untuk                          selalu terbuka dengan siapa pun, ya, Bela. Begitu kamu menarik                           diri, orang lain pun akan ragu untuk mendekatimu. Untuk kamu yang                        sudah memiliki pasangan, kamu mungkin akan menghadapi masalah                            serius dalam hubungan. Ingatlah untuk selalu berpikir panjang                            sebelum ambil keputusan penting."<<endl;
+            cout<<"Ramalan Percintaan: Bagi kamu Zodiak Pisces yang sedang single, kesempatan kamu untuk menemukan pasangan di tahun ini begitu besar. Jangan lupa untuk selalu terbuka dengan siapa pun, ya. Begitu kamu menarik diri, orang lain pun akan ragu untuk mendekatimu. Untuk kamu yang udah memiliki pasangan, kamu mungkin akan menghadapi masalah serius dalam hubungan. Ingatlah untuk selalu berpikir panjang sebelum ambil keputusan penting."<<endl;
                 break;
         case 4:
             cout<<"Aries"<<endl;
@@ -59,7 +67,7 @@ int main ()
             cout<<"Hari keberuntungan: 26 Maret 2021"<<endl;
             cout<<"Warna kesukaan    : Merah dan Kuning"<<endl;
             cout<<"Ramalan Keuangan  : Jangan memaksakan diri jika memang dana anda pas-pasan, lebih baik anda simpan dulu."<<endl;
-            cout<<"Ramalan Percintaan: Zodiak Aries yang masih single, kayaknya belum bisa menetap di satu tempat, nih. Jiwa petualang kamu lagi ingin mengembara dan                          mengenal dengan banyak orang sebelum mulai berkomitmen. Sedangkan                        kamu yang sudah memiliki pasangan, sepertinya sedang menikmati                           kehangatan hubunganmu. Selamat, ya!"<<endl;
+            cout<<"Ramalan Percintaan: Zodiak Aries yang masih single, kayaknya belum bisa menetap di satu tempat, nih. Jiwa petualang kamu lagi ingin mengembara dan mengenal dengan banyak orang sebelum mulai berkomitmen. Sedangkan kamu yang sudah memiliki pasangan, sepertinya sedang menikmati kehangatan hubunganmu. Selamat, ya!"<<endl;
                 break;
         case 5:
             cout<<"Taurus"<<endl;
@@ -67,23 +75,23 @@ int main ()
             cout<<"Hari keberuntungan: 17 Januari 2021"<<endl;
             cout<<"Warna kesukaan    : Cokelat dan Putih"<<endl;
             cout<<"Ramalan Keuangan  : Masih perlu banyak berhemat jika tidak ingin pusing dan bisa hidup tenang."<<endl;
-            cout<<"Ramalan Percintaan: Untuk Zodiak Taurus di awal tahun, kehidupan percintaan sepertinya akan terasa berat untukmu. Tapi tenang, di paruh kedua                        tahun kamu akan merasa bahwa masalah percintaanmu pelan-pelan                            mulai terangkat. Bagi kamu yang sudah memiliki pasangan, kini                            saatnya kamu mengambil kontrol dalam hubunganmu. Jangan terlalu                          banyak mengalah, karena hal itu juga nggak baik untuk hubunganmu                         dengannya."<<endl;
+            cout<<"Ramalan Percintaan: Untuk Zodiak Taurus di awal tahun, kehidupan percintaan sepertinya akan terasa berat untukmu. Tapi tenang, di paruh kedua tahun kamu akan merasa bahwa masalah percintaanmu pelan-pelan mulai terangkat. Bagi kamu yang sudah memiliki pasangan, kini saatnya kamu mengambil kontrol dalam hubunganmu. Jangan terlalu banyak mengalah, karena hal itu juga nggak baik untuk hubunganmu dengannya."<<endl;
                 break;
         case 6:
             cout<<"Gemini"<<endl;
             cout<<endl;
             cout<<"Hari keberuntungan: 31 Mei 2021"<<endl;
             cout<<"Warna kesukaan    : Jingga dan Kuning"<<endl;
-            cout<<"Ramalan Keuangan  : Karena pemborosan serasa sulit untuk diredam maka jalan satu-satunya tetap berhemat dan melakukan pengetatan di segala                           sektor."<<endl;
-            cout<<"Ramalan Percintaan: Zodiak Gemini, kamu akan menikmati kehidupan cintamu di tahun ini. Pada akhirnya kamu akan bertemu seseorang yang membuatmu                            berbunga-bunga. Sementara buat yang sudah memiliki pasangan,                             tahun ini adalah saatnya kamu menambahkan bumbu-bumbu sensualitas                        dalam hubunganmu"<<endl;
+            cout<<"Ramalan Keuangan  : Karena pemborosan serasa sulit untuk diredam maka jalan satu-satunya tetap berhemat dan melakukan pengetatan di segala sektor."<<endl;
+            cout<<"Ramalan Percintaan: Zodiak Gemini, kamu akan menikmati kehidupan cintamu di tahun ini. Pada akhirnya kamu akan bertemu seseorang yang membuatmu berbunga-bunga. Sementara buat yang sudah memiliki pasangan, tahun ini adalah saatnya kamu menambahkan bumbu-bumbu sensualitas dalam hubunganmu"<<endl;
                 break;
         case 7:
             cout<<"Cancer"<<endl;
             cout<<endl;
             cout<<"Hari keberuntungan: 23 Juni 2021"<<endl;
             cout<<"Warna kesukaan    : Silver dan Biru"<<endl;
-            cout<<"Ramalan Keuangan  : Tetap perlu memanage keuangan anda sebaik mungkin. Setiap pengeluaran sekecil apapun sebelum berjalan terus kontrol dengan                         baik."<<endl;
-            cout<<"Ramalan Percintaan: Untuk Zodiak Cancer, tahun ini juga cukup menarik untukmu. Jika kamu menginginkan hubungan jangka panjang, kamu bisa                                     mendapatkannya di tahun ini. Di sisi lain, jika kamu justru ingin                        mendapatkan hubungan singkat yang hanya untuk bersenang-senang,                          kamu juga bisa dengan mudah mendapatkannya. Jadi sebenarnya, itu                         semua tergantung padamu, ingin yang singkat atau jangka                                  panjang."<<endl;
+            cout<<"Ramalan Keuangan  : Tetap perlu memanage keuangan anda sebaik mungkin. Setiap pengeluaran sekecil apapun sebelum berjalan terus kontrol dengan baik."<<endl;
+            cout<<"Ramalan Percintaan: Untuk Zodiak Cancer, tahun ini juga cukup menarik untukmu. Jika kamu menginginkan hubungan jangka panjang, kamu bisa mendapatkannya di tahun ini. Di sisi lain, jika kamu justru ingin mendapatkan hubungan singkat yang hanya untuk bersenang-senang, kamu juga bisa dengan mudah mendapatkannya. Jadi sebenarnya, itu semua tergantung padamu, ingin yang singkat atau jangka panjang."<<endl;
                 break;
         case 8:
             cout<<"Leo"<<endl;
@@ -91,41 +99,41 @@ int main ()
             cout<<"Hari keberuntungan: 13 Juli 2021"<<endl;
             cout<<"Warna kesukaan    : Kuning dan Emas"<<endl;
             cout<<"Ramalan Keuangan  : Banyak keuntungan yang bisa anda raih, hanya saja jangan cepat merasa puas dulu dengan apa yang telah diraih selama ini."<<endl;
-            cout<<"Ramalan Percintaan: Zodiak Leo, kamu ingin bersikap realistis tahun ini. Bukan lagi cari hubungan untuk senang-senang, kamu sedang mencari seorang                           pasangan hidup. Jadi bersabarlah, ada kemungkinan kamu                                   menemukannya tahun ini, tapi mungkin di akhir tahun. Untuk yang                          sudah punya pasangan, hati-hati dengan kecemburuan dalam hubungan                        yang bisa merusak perasaan kamu dan dia."<<endl;
+            cout<<"Ramalan Percintaan: Zodiak Leo, kamu ingin bersikap realistis tahun ini. Bukan lagi cari hubungan untuk senang-senang, kamu sedang mencari seorang pasangan hidup. Jadi bersabarlah, ada kemungkinan kamu menemukannya tahun ini, tapi mungkin di akhir tahun. Untuk yang sudah punya pasangan, hati-hati dengan kecemburuan dalam hubungan yang bisa merusak perasaan kamu dan dia."<<endl;
                 break;
         case 9:
             cout<<"Virgo"<<endl;
             cout<<endl;
             cout<<"Hari keberuntungan: 12 Juli 2021"<<endl;
             cout<<"Warna kesukaan    : Hijau dan Abu-abu"<<endl;
-            cout<<"Ramalan Keuangan  : Kurang begitu bagus dan masih perlu menelaah mana-mana yang harus                        dikerjakan dulu dan mana yang bisa ditangguhkan."<<endl;
-            cout<<"Ramalan Percintaan: Pengaruh bintang-bintang di tahun ini menyebabkan Zodiak Virgo                           bisa memikat banyak orang dalam sekaligus. Tapi jangan bermain                           api, ya, bisa-bisa kamu yang harus menelan pil pahitnya, Bela.                           Untuk mereka yang sudah punya pasangan, hati-hati dalam bertindak                        dan berbicara. Pasangan kamu sepertinya lagi sensitif dan hal itu                        bisa memengaruhi hubungan kalian."<<endl;
+            cout<<"Ramalan Keuangan  : Kurang begitu bagus dan masih perlu menelaah mana-mana yang harus dikerjakan dulu dan mana yang bisa ditangguhkan."<<endl;
+            cout<<"Ramalan Percintaan: Pengaruh bintang-bintang di tahun ini menyebabkan Zodiak Virgo bisa memikat banyak orang dalam sekaligus. Tapi jangan bermain api, ya, bisa-bisa kamu yang harus menelan pil pahitnya. Untuk mereka yang sudah punya pasangan, hati-hati dalam bertindak dan berbicara. Pasangan kamu sepertinya lagi sensitif dan hal itu bisa memengaruhi hubungan kalian."<<endl;
                 break;
         case 10:
             cout<<"Libra"<<endl;
             cout<<endl;
             cout<<"Hari keberuntungan: 6 September 2021"<<endl;
             cout<<"Warna kesukaan    : Biru dan Pink"<<endl;
-            cout<<"Ramalan Keuangan  : Jangan cemas dengan kebutuhan yang cukup banyak, karena pemasukan                        masih bisa diandalkan"<<endl;
-            cout<<"Ramalan Percintaan: Awal tahun ini adalah waktu yang tepat untuk Zodiak Libra                                melangkah ke jenjang selanjutnya, apakah itu bertunangan atau                            menikah. Sementara buat kamu yang sedang mencari cinta, maka                             cobalah mencari di tempat yang tak biasanya kamu kunjungi dan                            berkenalan dengan orang baru. Cinta pada pandangan pertama bisa                          terjadi padamu di tahun ini!"<<endl;
+            cout<<"Ramalan Keuangan  : Jangan cemas dengan kebutuhan yang cukup banyak, karena pemasukan masih bisa diandalkan"<<endl;
+            cout<<"Ramalan Percintaan: Awal tahun ini adalah waktu yang tepat untuk Zodiak Libra melangkah ke jenjang selanjutnya, apakah itu bertunangan atau menikah. Sementara buat kamu yang sedang mencari cinta, maka cobalah mencari di tempat yang tak biasanya kamu kunjungi dan berkenalan dengan orang baru. Cinta pada pandangan pertama bisa terjadi padamu di tahun ini!"<<endl;
                 break;
         case 11:
             cout<<"Scorpio"<<endl;
             cout<<endl;
             cout<<"Hari keberuntungan: 17 November 2021"<<endl;
             cout<<"Warna kesukaan    : Hitam dan Merah"<<endl;
-            cout<<"Ramalan Keuangan  : Walau pemasukan ada hambatan, akan tetapi keuangan anda relatif                          stabil."<<endl;
-            cout<<"Ramalan Percintaan: Untuk kamu Zodiak Scorpio memang mengharapkan kebebasan. Namun                           setelah mendapatkannya, kamu justru merasa kesepian. Tapi tenang                         saja, pada akhirnya kamu akan meraih kebahagiaanmu, ketika kamu                          tidak lagi fokus untuk mendapatkan pacar. Buat yang sudah punya                          pasangan, hati-hati saat menggabungkan cinta dan bisnis, hal itu                         bisa merusak salah satunya"<<endl;
+            cout<<"Ramalan Keuangan  : Walau pemasukan ada hambatan, akan tetapi keuangan anda relatif stabil."<<endl;
+            cout<<"Ramalan Percintaan: Untuk kamu Zodiak Scorpio memang mengharapkan kebebasan. Namun setelah mendapatkannya, kamu justru merasa kesepian. Tapi tenang saja, pada akhirnya kamu akan meraih kebahagiaanmu, ketika kamu tidak lagi fokus untuk mendapatkan pacar. Buat yang sudah punya pasangan, hati-hati saat menggabungkan cinta dan bisnis, hal itu bisa merusak salah satunya"<<endl;
                 break;
         case 12:
             cout<<"Sagitarius"<<endl;
             cout<<endl;
             cout<<"Hari keberuntungan: 21 Mei 2021"<<endl;
             cout<<"Warna kesukaan    : Ungu dan Jingga"<<endl;
-            cout<<"Ramalan Keuangan  : Jangan segan untuk memberinya jika memang ada akan tetapi jika                           memang tak ada maka tak perlu malu untuk mengutarakannya"<<endl;
-            cout<<"Ramalan Percintaan: Buka matamu lebar-lebar jika ingin menemukan cinta yang baru.                            Kadang orang yang tepat untukmu sebenarnya sudah ada di depan                            mata sejak lama. Bagi Zodiak Sagitarius yang sudah memiliki                              pasangan, kamu sepertinya mulai meragukan pasanganmu, padahal                            prasangka burukmu itu tidak beralasan. Ingat, kepercayaan itu                            adalah yang paling penting dalam hubungan"<<endl;
+            cout<<"Ramalan Keuangan  : Jangan segan untuk memberinya jika memang ada akan tetapi jika memang tak ada maka tak perlu malu untuk mengutarakannya"<<endl;
+            cout<<"Ramalan Percintaan: Buka matamu lebar-lebar jika ingin menemukan cinta yang baru. Kadang orang yang tepat untukmu sebenarnya sudah ada di depan sejak lama. Bagi Zodiak Sagitarius yang sudah memiliki pasangan, kamu sepertinya mulai meragukan pasanganmu, padahal prasangka burukmu itu tidak beralasan. Ingat, kepercayaan itu adalah yang paling penting dalam hubungan"<<endl;
                 break;
-
     }
+    cout<<endl;
+    cout<<endl;
 }
-
